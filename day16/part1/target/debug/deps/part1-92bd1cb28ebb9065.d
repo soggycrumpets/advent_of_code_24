cd0262664a54298a8059c285c9df9492fa2dad7b
root <root@Lumbridge>
2025-03-06 16:56:44 -0500

    Day 16 part 1 nearly done

---
+++ day16/part1/target/debug/deps/part1-92bd1cb28ebb9065.d
@@ -0,0 +1,5 @@
+/root/rust_programs/advent_of_code_2024/day16/part1/target/debug/deps/part1-92bd1cb28ebb9065: src/main.rs
+
+/root/rust_programs/advent_of_code_2024/day16/part1/target/debug/deps/part1-92bd1cb28ebb9065.d: src/main.rs
+
+src/main.rs:
