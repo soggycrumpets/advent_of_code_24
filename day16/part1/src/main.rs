59657957e152fac3456b7ab0bd91d25a3e8ffc04
root <root@Lumbridge>
2025-03-06 19:19:52 -0500

    Not actually close to solving Day 15 part 1

--- day16/part1/src/main.rs
+++ day16/part1/src/main.rs
@@ -1,7 +1,6 @@
 use std::fmt;
 use std::fs::File;
 use std::io::prelude::*;
-use std::ops::Add;
 use std::path::Path;
 
 const _INPUT: &str = "input.txt";
@@ -9,43 +8,44 @@ const _EXAMPLE_1: &str = "example1.txt";
 const _EXAMPLE_2: &str = "example2.txt";
 const _TEST_STRAIGHT_SHOT: &str = "test_straight_shot.txt";
 const _TEST_AROUND_CORNER: &str = "test_around_corner.txt";
+const _TEST_BASE_CASE: &str = "test_base_case.txt";
 
 #[derive(PartialEq, Clone, Copy, Debug, Eq, Hash)]
-struct Vector2d {
+struct Coordinate2d {
     x: i32,
     y: i32,
 }
-impl Vector2d {
-    fn left(&self) -> Vector2d {
-        Vector2d {
+impl Coordinate2d {
+    fn west(&self) -> Coordinate2d {
+        Coordinate2d {
             x: self.x - 1,
             y: self.y,
         }
     }
-    fn right(&self) -> Vector2d {
-        Vector2d {
+    fn east(&self) -> Coordinate2d {
+        Coordinate2d {
             x: self.x + 1,
             y: self.y,
         }
     }
-    fn up(&self) -> Vector2d {
-        Vector2d {
+    fn north(&self) -> Coordinate2d {
+        Coordinate2d {
             x: self.x,
             y: self.y - 1,
         }
     }
-    fn down(&self) -> Vector2d {
-        Vector2d {
+    fn south(&self) -> Coordinate2d {
+        Coordinate2d {
             x: self.x,
             y: self.y + 1,
         }
     }
-    fn ahead(&self, direction: char) -> Vector2d {
+    fn ahead(&self, direction: char) -> Coordinate2d {
         match direction {
-            '^' => self.up(),
-            '>' => self.right(),
-            'v' => self.down(),
-            '<' => self.left(),
+            '^' => self.north(),
+            '>' => self.east(),
+            'v' => self.south(),
+            '<' => self.west(),
             _ => panic!(
                 "Tried to get next position, but direction is \"{}\"",
                 direction
@@ -53,16 +53,7 @@ impl Vector2d {
         }
     }
 }
-impl Add for Vector2d {
-    type Output = Self;
-    fn add(self, other: Self) -> Vector2d {
-        Vector2d {
-            x: self.x + other.x,
-            y: self.y + other.y,
-        }
-    }
-}
-impl fmt::Display for Vector2d {
+impl fmt::Display for Coordinate2d {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "({}, {})", self.x, self.y)
     }
@@ -70,11 +61,10 @@ impl fmt::Display for Vector2d {
 
 #[derive(PartialEq, Clone, Debug)]
 struct Reindeer {
-    position: Vector2d,
+    position: Coordinate2d,
     direction: char,
     score: i32,
     min_score: Option<i32>,
-    found_exit: bool,
 }
 impl Reindeer {
     fn new(maze: &Vec<Vec<char>>) -> Reindeer {
@@ -83,7 +73,6 @@ impl Reindeer {
             direction: '>',
             score: 0,
             min_score: None,
-            found_exit: false,
         }
     }
     fn move_straight(&mut self) {
@@ -97,7 +86,7 @@ impl Reindeer {
             'v' => '>',
             '>' => '^',
             _ => panic!(
-                "Tried to turn left, but direction is \"{}\"",
+                "Tried to face left, but direction is \"{}\"",
                 self.direction
             ),
         }
@@ -109,17 +98,13 @@ impl Reindeer {
             'v' => '<',
             '<' => '^',
             _ => panic!(
-                "Tried to turn right, but direction is \"{}\"",
+                "Tried to face right, but direction is \"{}\"",
                 self.direction
             ),
         }
     }
-    fn turn_left(&mut self) {
-        self.direction = self.left();
-        self.score += 1000;
-    }
-    fn turn_right(&mut self) {
-        self.direction = self.right();
+    fn turn(&mut self, direction: char) {
+        self.direction = direction;
         self.score += 1000;
     }
     fn merge_min_score(&mut self, new_min_score: Option<i32>) {
@@ -133,24 +118,24 @@ impl Reindeer {
     }
 }
 
-fn mark_map(map: &mut Vec<Vec<char>>, position: Vector2d, mark: char) {
+fn mark_map(map: &mut Vec<Vec<char>>, position: Coordinate2d, mark: char) {
     map[position.y as usize][position.x as usize] = mark;
 }
 
-fn path_is_clear(maze: &Vec<Vec<char>>, position: Vector2d) -> bool {
+fn path_is_clear(maze: &Vec<Vec<char>>, position: Coordinate2d) -> bool {
     match maze[position.y as usize][position.x as usize] {
         '#' | '<' | '>' | '^' | 'v' => false,
         _ => true,
     }
 }
 
-fn find_start_point(grid: &Vec<Vec<char>>) -> Vector2d {
+fn find_start_point(grid: &Vec<Vec<char>>) -> Coordinate2d {
     // Find the guard's starting position (initialize to shut up the compiler)
 
     for i in 0..grid.len() {
         for j in 0..grid[0].len() {
             if grid[i][j] == 'S' {
-                return Vector2d {
+                return Coordinate2d {
                     x: j as i32,
                     y: i as i32,
                 };
@@ -161,13 +146,14 @@ fn find_start_point(grid: &Vec<Vec<char>>) -> Vector2d {
 }
 
 fn _print_grid(grid: &Vec<Vec<char>>) {
-    for row in grid {
-        for c in row {
-            print!("{}", c);
+    let mut buf = String::new();
+    for i in grid {
+        for j in i {
+            buf.push(*j);
         }
-        println!();
+        buf.push('\n');
     }
-    println!();
+    print!("{}", buf);
 }
 
 fn read_file_to_array(name: &str) -> Vec<Vec<char>> {
@@ -183,7 +169,7 @@ fn read_file_to_array(name: &str) -> Vec<Vec<char>> {
     grid
 }
 
-fn found_exit(maze: &Vec<Vec<char>>, position: Vector2d) -> bool {
+fn found_exit(maze: &Vec<Vec<char>>, position: Coordinate2d) -> bool {
     if maze[position.y as usize][position.x as usize] == 'E' {
         true
     } else {
@@ -191,16 +177,22 @@ fn found_exit(maze: &Vec<Vec<char>>, position: Vector2d) -> bool {
     }
 }
 
-fn pathfind_new_instance(reindeer: &mut Reindeer, maze: &mut Vec<Vec<char>>) {
-    mark_map(maze, reindeer.position, reindeer.direction);
-    reindeer.move_straight();
-    pathfind(reindeer, maze);
+fn spawn_sub_reindeer(reindeer: &Reindeer, maze: &Vec<Vec<char>>, spawn_direction: char) -> Reindeer {
+    let mut new_reindeer = reindeer.clone();
+    new_reindeer.turn(spawn_direction);
+
+    let mut new_maze = maze.clone();
+    mark_map(&mut new_maze, new_reindeer.position, new_reindeer.direction);
+
+    new_reindeer.move_straight();
+    pathfind(&mut new_reindeer, &mut new_maze);
+
+    new_reindeer
 }
 
 fn pathfind(reindeer: &mut Reindeer, maze: &mut Vec<Vec<char>>) {
     // If the exit is found, this score is a potential minimum score. Merge it with the current minimum score
     if found_exit(maze, reindeer.position) {
-        reindeer.found_exit = true;
         reindeer.merge_min_score(Some(reindeer.score));
         println!("Score: {}", reindeer.score);
         _print_grid(maze);
@@ -210,22 +202,14 @@ fn pathfind(reindeer: &mut Reindeer, maze: &mut Vec<Vec<char>>) {
     // Mark this space on the map as visited
     mark_map(maze, reindeer.position, reindeer.direction);
 
-    // Spawn a new reindeer to explore left. Merge this reindeer's min score with the new one.
+    // Spawn a new reindeer to explore left. Merge this reindeer's min score with the current one.
     if path_is_clear(maze, reindeer.position.ahead(reindeer.left())) {
-        let mut new_reindeer = reindeer.clone();
-        new_reindeer.turn_left();
-        let mut new_maze = maze.clone();
-        pathfind_new_instance(&mut new_reindeer, &mut new_maze);
-
+        let new_reindeer = spawn_sub_reindeer(&reindeer, &maze, reindeer.left());
         reindeer.merge_min_score(new_reindeer.min_score);
     }
-    // Spawn a new reindeer to explore right. Merge this reindeer's min score with the new one.
+    // Spawn a new reindeer to explore right. Merge this reindeer's min score with the current one.
     if path_is_clear(maze, reindeer.position.ahead(reindeer.right())) {
-        let mut new_reindeer = reindeer.clone();
-        new_reindeer.turn_right();
-        let mut new_maze = maze.clone();
-        pathfind_new_instance(&mut new_reindeer, &mut new_maze);
-
+        let new_reindeer = spawn_sub_reindeer(&reindeer, &maze, reindeer.right());
         reindeer.merge_min_score(new_reindeer.min_score);
     }
     // Move reindeer ahead.
@@ -233,19 +217,22 @@ fn pathfind(reindeer: &mut Reindeer, maze: &mut Vec<Vec<char>>) {
         reindeer.move_straight();
         pathfind(reindeer, maze);
     }
+    // else {
+    // _print_grid(maze);
+    // }
 
     // The reindeer has no more moves to make
     return;
 }
 
 fn main() {
-    let mut maze = read_file_to_array(_EXAMPLE_1);
+    let mut maze = read_file_to_array(_INPUT);
     _print_grid(&maze);
     let mut reindeer = Reindeer::new(&maze);
 
     pathfind(&mut reindeer, &mut maze);
     if let Some(min_score) = reindeer.min_score {
-    println!("Min score: {}", min_score);
+        println!("Min score: {}", min_score);
     } else {
         panic!("Failed to find a minimum score!");
     }
@@ -261,45 +248,44 @@ mod tests {
     fn test_find_start() {
         let maze = read_file_to_array(_EXAMPLE_1);
         let start_point = find_start_point(&maze);
-        assert_eq!(start_point, Vector2d { x: 1, y: 13 });
+        assert_eq!(start_point, Coordinate2d { x: 1, y: 13 });
     }
 
     #[test]
     fn test_reindeer_methods() {
         let mut reindeer = Reindeer {
-            position: Vector2d { x: 5, y: 5 },
+            position: Coordinate2d { x: 5, y: 5 },
             direction: '^',
             score: 0,
             min_score: None,
-            found_exit: false,
         };
         reindeer.move_straight();
-        assert_eq!(reindeer.position, Vector2d { x: 5, y: 4 });
+        assert_eq!(reindeer.position, Coordinate2d { x: 5, y: 4 });
         assert_eq!(reindeer.score, 1);
 
-        reindeer.turn_left();
+        reindeer.turn(reindeer.left());
         assert_eq!(reindeer.direction, '<');
         assert_eq!(reindeer.score, 1001);
-        reindeer.turn_left();
+        reindeer.turn(reindeer.left());
         assert_eq!(reindeer.direction, 'v');
         assert_eq!(reindeer.score, 2001);
-        reindeer.turn_left();
+        reindeer.turn(reindeer.left());
         assert_eq!(reindeer.direction, '>');
         assert_eq!(reindeer.score, 3001);
-        reindeer.turn_left();
+        reindeer.turn(reindeer.left());
         assert_eq!(reindeer.direction, '^');
         assert_eq!(reindeer.score, 4001);
 
-        reindeer.turn_right();
+        reindeer.turn(reindeer.right());
         assert_eq!(reindeer.direction, '>');
         assert_eq!(reindeer.score, 5001);
-        reindeer.turn_right();
+        reindeer.turn(reindeer.right());
         assert_eq!(reindeer.direction, 'v');
         assert_eq!(reindeer.score, 6001);
-        reindeer.turn_right();
+        reindeer.turn(reindeer.right());
         assert_eq!(reindeer.direction, '<');
         assert_eq!(reindeer.score, 7001);
-        reindeer.turn_right();
+        reindeer.turn(reindeer.right());
         assert_eq!(reindeer.direction, '^');
         assert_eq!(reindeer.score, 8001);
     }
@@ -311,12 +297,12 @@ mod tests {
         let reindeer = Reindeer::new(&maze);
 
         assert_eq!(
-            path_is_clear(&maze, reindeer.position.left()),
+            path_is_clear(&maze, reindeer.position.west()),
             false,
             "Path check to the left failed"
         );
         assert_eq!(
-            path_is_clear(&maze, reindeer.position.right()),
+            path_is_clear(&maze, reindeer.position.east()),
             true,
             "Path check to the right failed"
         );
@@ -343,11 +329,19 @@ mod tests {
         assert_eq!(reindeer.min_score, Some(2018));
     }
 
-#[test]
-    fn test_website_example() {
-        let mut maze = read_file_to_array(_EXAMPLE_1);
-        let mut reindeer = Reindeer::new(&maze);
-        pathfind(&mut reindeer, &mut maze);
-        assert_eq!(reindeer.min_score, Some(7036));
+    #[test]
+    fn test_website_examples() {
+        {
+            let mut maze = read_file_to_array(_EXAMPLE_1);
+            let mut reindeer = Reindeer::new(&maze);
+            pathfind(&mut reindeer, &mut maze);
+            assert_eq!(reindeer.min_score, Some(7036), "Failed example 1!");
+        }
+        {
+            let mut maze = read_file_to_array(_EXAMPLE_2);
+            let mut reindeer = Reindeer::new(&maze);
+            pathfind(&mut reindeer, &mut maze);
+            assert_eq!(reindeer.min_score, Some(11048), "Failed example 2!");
+        }
     }
 }
