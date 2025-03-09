b768a9586fec30db8c29c729abb8024b54a29c07
root <root@Lumbridge>
2025-03-09 16:00:43 -0400

    Completed Day #17

--- day17/part2/src/main.rs
+++ day17/part2/src/main.rs
@@ -1,24 +1,23 @@
-use std::fmt;
 use std::fs::File;
 use std::io::prelude::*;
-use std::path::{Display, Path};
-use std::thread::sleep;
-use std::time::{Duration, Instant};
+use std::path::{Path};
+// use std::thread::sleep;
+// use std::time::{Duration, Instant};
 
 const _INPUT: &str = "input.txt";
 const _EXAMPLE1: &str = "part1_example.txt";
 const _EXAMPLE2: &str = "part2_example.txt";
 
 struct CPU<'a> {
-    rax: i128,
-    rbx: i128,
-    rcx: i128,
-    rip: i128,
+    rax: i64,
+    rbx: i64,
+    rcx: i64,
+    rip: i64,
 
-    output: &'a mut Vec<i128>,
+    output: &'a mut Vec<i64>,
 }
 impl<'a> CPU<'a> {
-    fn new(output: &'a mut Vec<i128>) -> CPU<'a> {
+    fn new(output: &'a mut Vec<i64>) -> CPU<'a> {
         CPU {
             rax: 0,
             rbx: 0,
@@ -28,7 +27,7 @@ impl<'a> CPU<'a> {
         }
     }
 
-    fn init(&mut self, rax: i128, rbx: i128, rcx: i128) {
+    fn init(&mut self, rax: i64, rbx: i64, rcx: i64) {
         self.rax = rax;
         self.rbx = rbx;
         self.rcx = rcx;
@@ -36,13 +35,13 @@ impl<'a> CPU<'a> {
         self.output.clear();
     }
 
-    fn run(&mut self, program: &Vec<i128>) {
-        while self.rip < program.len() as i128 {
+    fn run(&mut self, program: &Vec<i64>) {
+        while self.rip < program.len() as i64 {
             self.op(program);
         }
     }
 
-    fn op(&mut self, program: &Vec<i128>) {
+    fn op(&mut self, program: &Vec<i64>) {
         let opcode = program[self.rip as usize];
         let operand = program[self.rip as usize + 1];
 
@@ -59,7 +58,7 @@ impl<'a> CPU<'a> {
         }
     }
 
-    fn combo_operand(&self, operand: i128) -> i128 {
+    fn combo_operand(&self, operand: i64) -> i64 {
         match operand {
             0 => 0,
             1 => 1,
@@ -73,30 +72,30 @@ impl<'a> CPU<'a> {
         }
     }
 
-    fn adv(&mut self, operand: i128) {
+    fn adv(&mut self, operand: i64) {
         let numerator = self.rax;
-        let divisor: i128 = 1 << self.combo_operand(operand);
+        let divisor: i64 = 1 << self.combo_operand(operand);
         let result = numerator / divisor;
 
         self.rax = result;
         self.rip += 2;
     }
 
-    fn bxl(&mut self, operand: i128) {
+    fn bxl(&mut self, operand: i64) {
         let result = self.rbx ^ operand;
 
         self.rbx = result;
         self.rip += 2;
     }
 
-    fn bst(&mut self, operand: i128) {
+    fn bst(&mut self, operand: i64) {
         let result = self.combo_operand(operand) % 8;
 
         self.rbx = result;
         self.rip += 2;
     }
 
-    fn jnz(&mut self, operand: i128) {
+    fn jnz(&mut self, operand: i64) {
         if 0 != self.rax {
             self.rip = operand;
         } else {
@@ -104,32 +103,32 @@ impl<'a> CPU<'a> {
         }
     }
 
-    fn bxc(&mut self, _operand: i128) {
+    fn bxc(&mut self, _operand: i64) {
         let result = self.rbx ^ self.rcx;
 
         self.rbx = result;
         self.rip += 2;
     }
 
-    fn out(&mut self, operand: i128) {
+    fn out(&mut self, operand: i64) {
         let result = self.combo_operand(operand) % 8;
 
         self.output.push(result);
         self.rip += 2
     }
 
-    fn bdv(&mut self, operand: i128) {
+    fn bdv(&mut self, operand: i64) {
         let numerator = self.rax;
-        let divisor: i128 = 1 << self.combo_operand(operand);
+        let divisor: i64 = 1 << self.combo_operand(operand);
         let result = numerator / divisor;
 
         self.rbx = result;
         self.rip += 2;
     }
 
-    fn cdv(&mut self, operand: i128) {
+    fn cdv(&mut self, operand: i64) {
         let numerator = self.rax;
-        let divisor: i128 = 1 << self.combo_operand(operand);
+        let divisor: i64 = 1 << self.combo_operand(operand);
         let result = numerator / divisor;
 
         self.rcx = result;
@@ -138,27 +137,22 @@ impl<'a> CPU<'a> {
 }
 
 struct Controller<'a> {
-    target: &'a Vec<i128>,
-    state: State,
-    guess: i128,
-    matches: i32,
+    target: &'a Vec<i64>,
+    guess: i64,
     current_digit: usize,
-    digit_states: Vec<i128>,
+    digit_states: Vec<i64>,
 }
 impl<'a> Controller<'a> {
-    fn new(target: &Vec<i128>) -> Controller {
+    fn new(target: &Vec<i64>) -> Controller {
         Controller {
             target: target,
-            state: State::Forward,
-            guess: 8_i128.pow(target.len() as u32 - 1),
-            matches: 0,
+            guess: 8_i64.pow(target.len() as u32 - 1),
             current_digit: target.len() - 1,
             digit_states: vec![0; target.len()],
         }
     }
 
-    fn get_next_guess(&mut self, output: &Vec<i128>) {
-
+    fn get_next_guess(&mut self, output: &Vec<i64>) {
         if self.current_digit_is_maxed() {
             while self.current_digit_is_maxed() {
                 self.reset_current_digit();
@@ -170,55 +164,9 @@ impl<'a> Controller<'a> {
         } else {
             self.increment_digit();
         }
-
-        // match self.state {
-        //     State::Forward => {
-        //         if self.current_digit_is_maxed() {
-        //             while self.current_digit_is_maxed() {
-        //                 self.reset_current_digit();
-        //                 self.current_digit += 1;
-        //             }
-        //             self.increment_digit();
-        //             self.state = State::Backward;
-        //         } else if self.current_digit_matches(output) {
-        //             self.current_digit -= 1;
-        //         } else {
-        //             self.increment_digit();
-        //         }
-        //     }
-        //     State::Backward => {
-        //         if self.current_digit_is_maxed() {
-        //             self.reset_current_digit();
-        //             self.current_digit += 1;
-        //         } else if self.current_digit_matches(output) {
-        //             self.current_digit -= 1;
-        //             self.state = State::Forward;
-        //         }
-        //         else {
-        //             self.increment_digit();
-        //         }
-        //     }
-        // }
-
-        // current digit = last digit
-        // while current digit does not match, increment current digit.
-        // current digit = next digit
-        // ...
-        // if current digit is incremented > 7 times
-        // zero current digit
-        // current digit = previous digit
-        // increment current digit and while current digit does not match, increment current digit
-        // if current digit is incremented > 7 times
-        // zero current digit
-        // current digit = previus digit
-        // ...
-        // if current digit matches
-        // current digit = next digit
-        // while current does not match, increment current digit
-        // ...
     }
 
-    fn current_digit_matches(&mut self, output: &Vec<i128>) -> bool {
+    fn current_digit_matches(&mut self, output: &Vec<i64>) -> bool {
         output[self.current_digit] == self.target[self.current_digit]
     }
 
@@ -227,72 +175,18 @@ impl<'a> Controller<'a> {
     }
 
     fn reset_current_digit(&mut self) {
-        self.guess -= self.digit_states[self.current_digit] * 8_i128.pow(self.current_digit as u32);
+        self.guess -= self.digit_states[self.current_digit] * 8_i64.pow(self.current_digit as u32);
         self.digit_states[self.current_digit] = 0;
     }
 
     fn increment_digit(&mut self) {
-        self.guess += 8_i128.pow(self.current_digit as u32);
+        self.guess += 8_i64.pow(self.current_digit as u32);
         self.digit_states[self.current_digit] += 1;
     }
 
-    // fn count_matching_digits_from_front(&mut self, output: &Vec<i128>) -> i32 {
-    //     let mut matches = 0;
-    //     for i in 0..output.len() {
-    //         if output[i] == self.target[i] {
-    //             matches += 1;
-    //         } else {
-    //             break;
-    //         }
-    //     }
-    //     matches
-    // }
-
-    // fn count_matching_digits_from_back(&mut self, output: &Vec<i128>) -> i32 {
-    //     let mut matches = 0;
-    //     for i in (0..output.len()).rev() {
-    //         if output[i] == self.target[i] {
-    //             matches += 1;
-    //         } else {
-    //             break;
-    //         }
-    //     }
-    //     matches
-    // }
-
-    // fn all_but_first_two_digits_match(&mut self, output: &Vec<i128>) -> bool {
-    //     for i in 0..output.len() - 2 {
-    //         if output[output.len() - 1 - i] != self.target[self.target.len() - 1 - i] {
-    //             return false;
-    //         }
-    //     }
-    //     return true;
-    // }
-
-    // fn all_digits_match(&mut self, output: &Vec<i128>) -> bool {
-    //     for i in 0..output.len() {
-    //         if output[output.len() - 1 - i] != self.target[self.target.len() - 1 - i] {
-    //             return false;
-    //         }
-    //     }
-    //     return true;
-    // }
-}
-#[derive(PartialEq)]
-enum State {
-    Forward,
-    Backward,
-}
-impl fmt::Display for State {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            State::Forward => write!(f, "Forward"),
-            State::Backward => write!(f, "Backward"),
-        }
-    }
 }
 
-fn load_program<'a>(filename: &str, cpu: &mut CPU) -> Vec<i128> {
+fn load_program<'a>(filename: &str, cpu: &mut CPU) -> Vec<i64> {
     let path = Path::new(filename);
     let mut file = File::open(&path).unwrap();
     let mut data_string: String = String::new();
@@ -305,19 +199,19 @@ fn load_program<'a>(filename: &str, cpu: &mut CPU) -> Vec<i128> {
     let rcx = get_num_from_line(lines.next());
     cpu.init(rax, rbx, rcx);
     lines.next();
-    let program: Vec<i128> = get_instructions_from_line(lines.next());
+    let program: Vec<i64> = get_instructions_from_line(lines.next());
 
     return program;
 
-    fn get_num_from_line(line: Option<&str>) -> i128 {
+    fn get_num_from_line(line: Option<&str>) -> i64 {
         line.unwrap()
             .chars()
             .filter(|char| char.is_numeric())
             .collect::<String>()
-            .parse::<i128>()
+            .parse::<i64>()
             .unwrap()
     }
-    fn get_instructions_from_line(line: Option<&str>) -> Vec<i128> {
+    fn get_instructions_from_line(line: Option<&str>) -> Vec<i64> {
         line.unwrap()
             .chars()
             .filter(|c| c.is_numeric() || *c == ',')
@@ -328,7 +222,7 @@ fn load_program<'a>(filename: &str, cpu: &mut CPU) -> Vec<i128> {
     }
 }
 
-fn _print_output(output: &Vec<i128>) {
+fn _print_output(output: &Vec<i64>) {
     let mut buf = String::new();
     for num in output {
         buf.push_str(num.to_string().as_str());
@@ -349,10 +243,10 @@ fn main() {
 
     let rbx = cpu.rbx;
     let rcx = cpu.rcx;
-    let mut rax: i128 = cpu.rax;
+    let mut rax;
 
-    let interval = Duration::from_secs(1) / 30;
-    let mut next_time = Instant::now() + interval;
+    // let interval = Duration::from_secs(1) / 30;
+    // let mut next_time = Instant::now() + interval;
 
     loop {
         rax = controller.guess;
@@ -360,21 +254,18 @@ fn main() {
         cpu.init(rax, rbx, rcx);
         cpu.run(&program);
         _print_output(&cpu.output);
-        print!(" | {}", controller.matches);
-        print!(" | {}", controller.current_digit);
-        print!(" | {}", controller.state);
-        print!(" | {}", controller.current_digit_is_maxed());
-        println!(" | {}", controller.guess);
+        print!(" | Current Digit: {}", controller.current_digit);
+        println!(" | Register A: {}", controller.guess);
 
         if *cpu.output == program {
             break;
         }
         controller.get_next_guess(cpu.output);
 
-        sleep(next_time - Instant::now());
-        next_time += interval;
+        // sleep(next_time - Instant::now());
+        // next_time += interval;
     }
-    println!("Program copies itself for rax = {}", rax);
+    print!("\nProgram copies itself at: Register A = {}\n\n", rax);
 }
 
 #[test]
@@ -422,22 +313,32 @@ fn test_website_examples() {
 
 #[test]
 fn test_website_example_part2() {
-    let mut output = Vec::new();
+   let mut output = Vec::new();
     let mut cpu = CPU::new(&mut output);
+    let program = load_program(_INPUT, &mut cpu);
+    let mut controller: Controller = Controller::new(&program);
+    controller.current_digit = program.len() - 1;
 
-    let program = load_program(_EXAMPLE2, &mut cpu);
+    cpu.run(&program);
 
-    let mut rax = 1;
     let rbx = cpu.rbx;
     let rcx = cpu.rcx;
+    let mut rax;
 
     loop {
+        rax = controller.guess;
+
         cpu.init(rax, rbx, rcx);
         cpu.run(&program);
+        _print_output(&cpu.output);
+        print!("\t| Current Digit: {}", controller.current_digit);
+        println!("\t| Register A: {}", controller.guess);
+
         if *cpu.output == program {
             break;
         }
-        rax += 1;
+        controller.get_next_guess(cpu.output);
     }
+
     assert_eq!(rax, 117440);
 }
